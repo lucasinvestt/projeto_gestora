65cd32377d6e8fc2b20e8c58c7708568c588b893
Lucas Xavier <lucas.costa@investtools.com.br>
2022-02-22 17:50:33 -0300

    change property name from fund_base route return. totalvalue to balance

--- app/models/cash_transaction.rb
+++ app/models/cash_transaction.rb
@@ -2,7 +2,7 @@ class CashTransaction < ApplicationRecord
   belongs_to :fund
 
   def self.fund_balance(fund_id, date)
-    all.select("fund_id, sum(cash_transactions.value) as totalValue")
+    all.select("fund_id, sum(cash_transactions.value) as balance")
     .group("fund_id")
     .where(fund_id: fund_id)
     .where("date <= ?", date)
